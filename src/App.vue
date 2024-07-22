3eb06ec98e8fdbe1310c2b9c1e45dcea2be7b9ec
Cristian Sena <csena@gett.mobi>
2024-07-22 12:53:43 -0300

    Agrego select

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <script setup>
+import { ref } from "vue"
+
+
+const monedas = ref([
+      { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
+      { codigo: 'EUR', texto: 'Euro'},
+      { codigo: 'GBP', texto: 'Libra Esterlina'},
+      { codigo: 'ARS', texto: 'Peso Argentino'},
+      { codigo: 'MXN', texto: 'Peso Mexicano'},
+  ])
 
 </script>
 
 <template>
-
+  <div class="contenedor">
+    <h1 class="titulo">Cotizador de <span>Criptomonedas</span></h1>
+    <div class="contenido">
+      <form class="formulario">
+        <div class="campo">
+          <label for="moneda">Moneda</label>
+          <select id="moneda">
+            <option value="">-- Seleccione --</option>
+            <option 
+              v-for="moneda in monedas" 
+              :key="moneda.codigo" 
+              :value="moneda.codigo"
+            >{{ moneda.texto }}
+            </option>
+          </select>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
